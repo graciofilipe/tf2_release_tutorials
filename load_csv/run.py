d4a1f292ed92a0d8d744ca55188b7f72e4a80aec
graciofilipe <filipe@kanthaka.com>
2019-10-06 21:42:58 +0100

    load csv

---
+++ load_csv/run.py
@@ -0,0 +1,111 @@
+import functools
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+
+TRAIN_DATA_URL = "https://storage.googleapis.com/tf-datasets/titanic/train.csv"
+TEST_DATA_URL = "https://storage.googleapis.com/tf-datasets/titanic/eval.csv"
+
+train_file_path = tf.keras.utils.get_file("train.csv", TRAIN_DATA_URL)
+test_file_path = tf.keras.utils.get_file("eval.csv", TEST_DATA_URL)
+print('train file path:', train_file_path)
+# Make numpy values easier to read.
+np.set_printoptions(precision=3, suppress=True)
+
+
+LABEL_COLUMN = 'survived'
+LABELS = [0, 1]
+def get_dataset(file_path, **kwargs):
+  dataset = tf.data.experimental.make_csv_dataset(
+      file_path,
+      batch_size=5, # Artificially small to make examples easier to show.
+      label_name=LABEL_COLUMN,
+      na_value="?",
+      num_epochs=1,
+      ignore_errors=True,
+      **kwargs)
+  return dataset
+
+raw_train_data = get_dataset(train_file_path)
+raw_test_data = get_dataset(test_file_path)
+
+def show_batch(dataset):
+  for batch, label in dataset.take(1):
+    for key, value in batch.items():
+      print("{:20s}: {}".format(key,value.numpy()))
+
+show_batch(raw_train_data)
+
+
+
+class PackNumericFeatures(object):
+  def __init__(self, names):
+    self.names = names
+
+  def __call__(self, features, labels):
+    numeric_freatures = [features.pop(name) for name in self.names]
+    numeric_features = [tf.cast(feat, tf.float32) for feat in numeric_freatures]
+    numeric_features = tf.stack(numeric_features, axis=-1)
+    features['numeric'] = numeric_features
+
+    return features, labels
+
+NUMERIC_FEATURES = ['age','n_siblings_spouses','parch', 'fare']
+packed_train_data = raw_train_data.map(PackNumericFeatures(NUMERIC_FEATURES))
+packed_test_data = raw_test_data.map(PackNumericFeatures(NUMERIC_FEATURES))
+
+desc = pd.read_csv(train_file_path)[NUMERIC_FEATURES].describe()
+MEAN = np.array(desc.T['mean'])
+STD = np.array(desc.T['std'])
+def normalize_numeric_data(data, mean, std):
+  # Center the data
+  return (data-mean)/std
+
+# See what you just created.
+normalizer = functools.partial(normalize_numeric_data, mean=MEAN, std=STD)
+
+numeric_column = tf.feature_column.numeric_column('numeric', normalizer_fn=normalizer, shape=[len(NUMERIC_FEATURES)])
+numeric_columns = [numeric_column]
+print('numeric column', numeric_column)
+
+
+CATEGORIES = {
+    'sex': ['male', 'female'],
+    'class' : ['First', 'Second', 'Third'],
+    'deck' : ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'],
+    'embark_town' : ['Cherbourg', 'Southhampton', 'Queenstown'],
+    'alone' : ['y', 'n']
+}
+
+categorical_columns = []
+for feature, vocab in CATEGORIES.items():
+  cat_col = tf.feature_column.categorical_column_with_vocabulary_list(
+        key=feature, vocabulary_list=vocab)
+  categorical_columns.append(tf.feature_column.indicator_column(cat_col))
+
+print('categorical columns', categorical_columns)
+
+### the columns for the model ##
+numeric_layer = tf.keras.layers.DenseFeatures(numeric_columns)
+categorical_layer = tf.keras.layers.DenseFeatures(categorical_columns)
+preprocessing_layer = tf.keras.layers.DenseFeatures(categorical_columns+numeric_columns)
+
+
+model = tf.keras.Sequential([
+  preprocessing_layer,
+  tf.keras.layers.Dense(64, activation='relu'),
+  tf.keras.layers.Dense(64, activation='relu'),
+  tf.keras.layers.Dense(1, activation='sigmoid'),
+])
+
+model.compile(
+    loss='binary_crossentropy',
+    optimizer='adam',
+    metrics=['accuracy'])
+
+train_data = packed_train_data.shuffle(500)
+test_data = packed_test_data
+
+early_stop = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=6)
+
+model.fit(train_data, validation_data=test_data, epochs=66, callbacks=[early_stop])
--- regression/run.py
+++ regression/run.py
@@ -1,1 +1,88 @@
-# memory error
+# https://www.tensorflow.org/tutorials/keras/regression
+
+import pathlib
+import pandas as pd
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+print(tf.__version__)
+
+dataset_path = keras.utils.get_file("auto-mpg.data", "http://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data")
+dataset_path
+
+column_names = ['MPG','Cylinders','Displacement','Horsepower','Weight',
+                'Acceleration', 'Model Year', 'Origin']
+raw_dataset = pd.read_csv(dataset_path, names=column_names,
+                      na_values = "?", comment='\t',
+                      sep=" ", skipinitialspace=True)
+
+dataset = raw_dataset.copy()
+dataset.tail()
+
+dataset.isna().sum()
+
+dataset = dataset.dropna()
+
+origin = dataset.pop('Origin')
+
+dataset['USA'] = (origin == 1)*1.0
+dataset['Europe'] = (origin == 2)*1.0
+dataset['Japan'] = (origin == 3)*1.0
+dataset.tail()
+
+train_dataset = dataset.sample(frac=0.8,random_state=0)
+test_dataset = dataset.drop(train_dataset.index)
+
+
+train_stats = train_dataset.describe()
+train_stats.pop("MPG")
+train_stats = train_stats.transpose()
+
+
+train_labels = train_dataset.pop('MPG')
+test_labels = test_dataset.pop('MPG')
+
+
+def norm(x):
+  return (x - train_stats['mean']) / train_stats['std']
+normed_train_data = norm(train_dataset)
+normed_test_data = norm(test_dataset)
+
+
+def build_model():
+  model = keras.Sequential([
+    layers.Dense(64, activation='relu', input_shape=[len(train_dataset.keys())]),
+    layers.Dense(64, activation='relu'),
+    layers.Dense(1)
+  ])
+
+  optimizer = tf.keras.optimizers.RMSprop(0.001)
+
+  model.compile(loss='mse',
+                optimizer=optimizer,
+                metrics=['mae', 'mse'])
+  return model
+
+model = build_model()
+
+
+
+# Display training progress by printing a single dot for each completed epoch
+class PrintDot(keras.callbacks.Callback):
+  def on_epoch_end(self, epoch, logs):
+    if epoch % 100 == 0: print('')
+    print('.', end='')
+
+EPOCHS = 1000
+
+# The patience parameter is the amount of epochs to check for improvement
+early_stop = keras.callbacks.EarlyStopping(monitor='val_loss', patience=10)
+
+history = model.fit(normed_train_data, train_labels, epochs=EPOCHS,
+                    validation_split = 0.2, verbose=0, callbacks=[early_stop, PrintDot()])
+
+
+loss, mae, mse = model.evaluate(normed_test_data, test_labels, verbose=2)
+
+print("Testing set Mean Abs Error: {:5.2f} MPG".format(mae))
