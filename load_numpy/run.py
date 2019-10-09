26752aef5271cd7592994db9954e45b11c5c3a0c
Gracio <filipe.gracio@M801736.local>
2019-10-09 18:31:25 +0100

    load numpy

---
+++ load_numpy/run.py
@@ -0,0 +1,40 @@
+import numpy as np
+import tensorflow as tf
+
+DATA_URL = 'https://storage.googleapis.com/tensorflow/tf-keras-datasets/mnist.npz'
+
+path = tf.keras.utils.get_file('mnist.npz', DATA_URL)
+with np.load(path) as data:
+  train_examples = data['x_train']
+  train_labels = data['y_train']
+  test_examples = data['x_test']
+  test_labels = data['y_test']
+
+
+print(test_labels.shape)
+print(test_examples.shape)
+
+train_dataset = tf.data.Dataset.from_tensor_slices((train_examples, train_labels))
+test_dataset = tf.data.Dataset.from_tensor_slices((test_examples, test_labels))
+
+BATCH_SIZE = 64
+SHUFFLE_BUFFER_SIZE = 100
+
+train_dataset = train_dataset.shuffle(SHUFFLE_BUFFER_SIZE).batch(BATCH_SIZE)
+test_dataset = test_dataset.batch(BATCH_SIZE)
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(45, activation='relu'),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
+
+model.compile(optimizer=tf.keras.optimizers.RMSprop(),
+                loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+                metrics=[tf.keras.metrics.SparseCategoricalAccuracy()])
+
+early_stop = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=5)
+
+model.fit(train_dataset, epochs=22, callbacks=[early_stop])
+
+model.evaluate(test_dataset)
